1ae1841fb9c8b3350598820c1b31e05489dd018a
unknown <murmuparmeshwar05@gmail.com>
2022-12-25 14:11:45 +0530

    Problem1-Problem5

---
+++ Day1_Introduction_To_Variables.js
@@ -0,0 +1,34 @@
+//Problem 1
+let str1 = "Masai School";
+let str2 = "A Transformation In Education";
+console.log(str1);
+console.log(str2);
+
+
+//Problem 2
+
+let name = "Parmeshwar Murmu";
+console.log(name);
+name = "Purno Chandra Murmu";
+console.log(name);
+name = "Sita Rani Murmu";
+console.log(name);
+
+//Problem 3
+let name = "Parmeshwar Murmu";
+let age = 23;
+console.log(name, age);
+console.log(typeof(name), typeof(age));
+
+//Problem 4
+
+let name = "Rahul";
+let school = "Kerala Public School";
+let grade = "A";
+let section = "B";
+let rollNo = 23;
+console.log("Name Of Student : " + name);
+console.log("School Name : " + school);
+console.log("Grade Scored : " + grade);
+console.log("Section : " + section);
+console.log("RollNo : "+ rollNo);
