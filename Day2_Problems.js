af51fb00c29c2082095b7ecf0ac6a23707a72f33
unknown <murmuparmeshwar05@gmail.com>
2022-12-25 14:27:07 +0530

    Day2_Mathematicla_Operator

---
+++ Day2_Problems.js
@@ -0,0 +1,52 @@
+// Problem
+
+console.log("Masai School" +"\n" + "A Transformation in Education");
+
+//Problem
+
+function multiplyBy50(number) {
+    // Write code here
+    console.log(number*50);
+}
+
+
+//Problem 
+
+function productOfSix(one, two, three, four, five, six) {
+    // Write code here
+    console.log(one*two*three*four*five*six);
+}
+
+//Problem
+
+function sumOfFive(one, two, three, four, five) {
+    // Write code here
+    console.log(one+two+three+four+five);
+}
+
+
+//Problem
+
+function cubeOfNumber(N) {
+    // Write code here
+    let result = N**3;
+    console.log(result);
+}
+
+//Problem
+
+function squareAndSum(one, two, three) {
+  // Write code here
+  let result = one**2 + two**2 + three**2;
+  console.log(result);
+}
+
+//Problem
+
+function findX(X) {
+    // Write code here
+    let mul = X*3;
+    let result = mul + 10;
+    console.log(result);
+    
+}
